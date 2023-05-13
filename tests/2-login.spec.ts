560a346f3707a6adeccf33318e9075d699ff18a8
Mani Movassagh <mani.mgh@gmail.com>
2023-05-13 14:09:55 +0200

    reusable login

--- tests/2-login.spec.ts
+++ tests/2-login.spec.ts
@@ -3,10 +3,10 @@ import { login } from '../Utilities/LoginReusable';
 /**
  * @description login to saudemo webiste and check the titile of page
  */
-test('login to sauce demo website', async ({ page }) => {
- login(page)
-  const pageLogo = page.locator(".app_logo")
-  expect(await pageLogo.textContent()).toContain("Swag Labs")
+test('login to sauce demo website but with reusable login', async ({ page }) => {
+    login(page)
+    const pageLogo = page.locator(".app_logo")
+    expect(await pageLogo.textContent()).toContain("Swag Labs")
 
 });
 
