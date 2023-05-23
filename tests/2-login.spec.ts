0d06c31d11bb0d232b251aa84221ccc139f6fcd0
Mani Movassagh <mani.mgh@gmail.com>
2023-05-23 23:12:35 +0200

    tabnine

--- tests/2-login.spec.ts
+++ tests/2-login.spec.ts
@@ -7,6 +7,8 @@ test('login to sauce demo website but with reusable login', async ({ page }) => 
     login(page)
     const pageLogo = page.locator(".app_logo")
     expect(await pageLogo.textContent()).toContain("Swag Labs")
+    function login(page) {  
+        return   page.goto('https://www.saucedemo.com/')}
 
 });
 
